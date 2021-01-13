e70841eba5a5780f25fb479f839c034349e4a473
VaidaEG <grausliene.vaida@gmail.com>
2021-01-13 21:12:45 +0200

    Filter, map, sort

--- js/main.js
+++ js/main.js
@@ -4,18 +4,20 @@ console.log('Labas');
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1800's
-const eighteen = inventors.filter(function(inventor) {
-    if(inventor.year >= 1800 && inventor.year < 1900) {
-        return true; // keep it!
-    }
-});
+const eighteen = inventors.filter(inventor => (inventor.year >= 1800 && inventor.year < 1900));
+
 console.table(eighteen);
 
 // Array.prototype.map()
 // 2. Gives us an array of the inventors first and last names
+const fullNames = inventors.map(inventor => inventor.name + ' ' + inventor.last);
+console.log(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+
+console.log(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
