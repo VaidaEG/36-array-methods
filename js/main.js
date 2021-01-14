29816484b3ffc57d6e2abcf6f0eb19fd2580d947
VaidaEG <grausliene.vaida@gmail.com>
2021-01-14 13:56:44 +0200

    reduce, sort by year lived

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import { inventors, people } from './data.js';
 
-console.log('Labas');
-
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1800's
 const eighteen = inventors.filter(inventor => (inventor.year >= 1800 && inventor.year < 1900));
@@ -17,12 +15,23 @@ console.log(fullNames);
 // 3. Sort the inventors by birthdate, oldest to youngest
 const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
 
-console.log(ordered);
+console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
+const totalYears = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort(function(a, b) {
+    const lastGuy = a.passed - a.year;
+    const nextGuy = b.passed - b.year;
+    return lastGuy > nextGuy ? -1 : 1;
+});
+console.table(oldest);
 
 // 6. Create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
